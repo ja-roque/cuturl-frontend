2da661af46fbce4098baadb49828a5c9f4047a3c
Javier Roque <jarb95@gmail.com>
2019-04-25 02:20:42 -0600

    Added a navbar as a header for aesthetics

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,10 +1,24 @@
 import "./Header.scss";
 import React from "react";
+import { Link } from 'react-router-dom'
+
+
 
 function template() {
   return (
     <div className="header">
-      <h1>Header</h1>
+      <nav class="navbar navbar-expand-lg navbar-dark">
+        <h1 class="navbar-brand" >CUT-URL</h1>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">          
+            <Link class="nav-item nav-link active"  to='/'>Home <span class="sr-only">(current)</span></Link>
+            <Link class="nav-item nav-link"  to={`/About`}>About</Link>            
+          </div>
+        </div>
+      </nav>
     </div>
   );
 };
