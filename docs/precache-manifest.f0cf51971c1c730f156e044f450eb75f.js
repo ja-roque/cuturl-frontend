7a1f25d2df35d35fb91263427f27a53fe9303c09
Javier Roque <jarb95@gmail.com>
2019-04-25 12:20:54 -0600

    Missing doc files

---
+++ docs/precache-manifest.f0cf51971c1c730f156e044f450eb75f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64397c0fbf117735d1543671f4632cb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f94327a838c778f1026d",
+    "url": "/static/css/main.c42bdc16.chunk.css"
+  },
+  {
+    "revision": "a38c21c20cb25e23266e",
+    "url": "/static/js/2.80596084.chunk.js"
+  },
+  {
+    "revision": "f94327a838c778f1026d",
+    "url": "/static/js/main.341dc518.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2de160cad9bfb4a6b299140d5eacb2ee",
+    "url": "/static/media/bg.2de160ca.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
